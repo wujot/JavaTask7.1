3f051cc4f0ca01e622c67263f213dc34d4ea599c
wujot <wojciech.wujot"gmail.com>
2018-02-05 15:29:56 +0100

    Variables were changed for static final according to instructions from slack.

--- src/CalendarConverter.java
+++ src/CalendarConverter.java
@@ -1,36 +1,36 @@
 public class CalendarConverter {
 
-    int monday = 1;
-    int tuesday = 2;
-    int wednesday = 3;
-    int thursday = 4;
-    int friday = 5;
-    int saturday = 6;
-    int sunday =7;
+    static final int MONDAY = 1;
+    static final int TUESDAY = 2;
+    static final int WEDNESDAY = 3;
+    static final int THURSDAY = 4;
+    static final int FRIDAY = 5;
+    static final int SATURDAY = 6;
+    static final int SUNDAY =7;
 
     String convertDayToString(int dayNumber) {
         String dayToString = null;
 
         switch (dayNumber) {
-            case 1:
+            case MONDAY:
                 dayToString = "Monday";
                 break;
-            case 2:
+            case TUESDAY:
                 dayToString = "Tuesday";
                 break;
-            case 3:
+            case WEDNESDAY:
                 dayToString = "Wednesday";
                 break;
-            case 4:
+            case THURSDAY:
                 dayToString = "Thursday";
                 break;
-            case 5:
+            case FRIDAY:
                 dayToString = "Friday";
                 break;
-            case 6:
+            case SATURDAY:
                 dayToString = "Saturday";
                 break;
-            case  7:
+            case  SUNDAY:
                 dayToString = "Sunday";
                 break;
                 default:
